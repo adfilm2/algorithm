cdeadef193f6b0f2f473c8d789ae5ccad8d3823c
dohan <adfilm2@naver.com>
2021-01-28 00:07:41 +0900

    [P]길찾기 게임

--- hye/9-길찾기게임.cpp
+++ hye/9-길찾기게임.cpp
@@ -3,18 +3,18 @@
 #include <vector>
 #include <algorithm>
 
-#define max(x,y) ((x)>(y))?(x):(y)
 using namespace std;
 
 struct node {
-    node(int x, int y, int num) : x(x), y(y), num(num){}
+    node(int x, int y, int num) : x(x), y(y), num(num), left(NULL), right(NULL){}
     node* left;
     node* right;
     int x,y,num;
-    
 };
 
 vector<node> nodeList;
+vector<int> preAns;
+vector<int> postAns;
 
 bool compare(node& a, node& b){
     if(a.y == b.y){
@@ -25,28 +25,45 @@ bool compare(node& a, node& b){
 }
 
 void add(node * p, node * c){
-    for(int i=0; i<nodeList.size(); i++){
-        if(nodeList[i+1].x < nodeList[i].x){
-            nodeList[i].left = &nodeList[i+1];
+    
+        if(c->x < p->x){
+            if(p->left == NULL){
+                p->left = c;
+            }else{
+                add(p->left, c);
+            }
+            
+        }else{
+            if(p->right == NULL){
+                p->right = c;
+            }else{
+                add(p->right, c);
+            }
         }
         
     }
+
+void pre(node * p){
+    if(p){
+        preAns.push_back(p->num);
+        pre(p->left);
+        pre(p->right);
+    }
 }
 
+void post(node * p){
+    if(p){
+        post(p->left);
+        post(p->right);
+        postAns.push_back(p->num);
+    }
+}
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
     vector<vector<int>> answer;
-    return answer;
-}
-
-
-int main(void){
-    vector<vector<int>> nodeinfo = {{5,3},{11,5},{13,3},{3,5},{6,1},{1,3},{8,6},{7,2},{2,2}};
     
-    int y=0;
     for(int i=0; i<nodeinfo.size(); i++){
         nodeList.push_back(node(nodeinfo[i][0],nodeinfo[i][1],i+1));
-        y = max(nodeinfo[i][1], y );
     }
     sort(nodeList.begin(), nodeList.end(), compare);
     
@@ -54,14 +71,63 @@ int main(void){
     for(int i=0 ;i<nodeList.size(); i++){
         cout << nodeList[i].x << " "<< nodeList[i].y << endl;
     }
+    node * root = &nodeList[0];
     
     for(int i=1; i<nodeList.size(); i++){
-        add(&nodeList[0], &nodeList[i]);
+        add(root, &nodeList[i]);
+    }
+    
+    pre(root);
+    post(root);
+    
+    answer.push_back(preAns);
+    answer.push_back(postAns);
+    
+    for(int i=0; i<2;i++){
+        for(int j=0; j<answer[0].size();j++){
+            cout << answer[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    
+    return answer;
+}
+
+
+int main(void){
+    vector<vector<int>> nodeinfo = {{5,3},{11,5},{13,3},{3,5},{6,1},{1,3},{8,6},{7,2},{2,2}};
+    vector<vector<int>> answer;
+    for(int i=0; i<nodeinfo.size(); i++){
+        nodeList.push_back(node(nodeinfo[i][0],nodeinfo[i][1],i+1));
+    }
+    sort(nodeList.begin(), nodeList.end(), compare);
+    
+    
+    for(int i=0 ;i<nodeList.size(); i++){
+        cout << nodeList[i].x << " "<< nodeList[i].y << endl;
+    }
+    node * root = &nodeList[0];
+    
+    for(int i=1; i<nodeList.size(); i++){
+        add(root, &nodeList[i]);
+    }
+    
+    pre(root);
+    post(root);
+    
+    answer.push_back(preAns);
+    answer.push_back(postAns);
+    
+    for(int i=0; i<2;i++){
+        for(int j=0; j<answer[0].size();j++){
+            cout << answer[i][j] << " ";
+        }
+        cout << endl;
     }
     
 }
 
  
-
 
 
